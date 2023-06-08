4765f4577a18638b6ef22aa3fbf08246fff208a3
Lucas Bortolo <lucasbortolo3@gmail.com>
2023-06-08 10:09:54 -0300

    inicial commit

---
+++ src/main/java/com/exercise/docker/backend/DockerExerciseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.docker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExerciseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerExerciseBackendApplication.class, args);
+    }
+
+}
